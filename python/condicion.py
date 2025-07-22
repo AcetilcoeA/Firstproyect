033d86950287db93c456122d4edda467ae8d9225
AcetilcoeA <henzedward@gmail.com>
2025-07-22 17:50:23 -0500

    Add age verification program to check if user is an adult

---
+++ python/condicion.py
@@ -0,0 +1,8 @@
+# Programa para verificar si eres mayor de edad
+
+edad = int(input("Introduce tu edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("No eres mayor de edad.")
